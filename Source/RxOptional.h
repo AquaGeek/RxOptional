a27d2964305ef75aba47965d1f15b8168e7161ba
Tyler Stromberg <tyler@uber.com>
2016-07-01 13:03:57 -0700

    Create Xcode project

---
+++ Source/RxOptional.h
@@ -0,0 +1,19 @@
+//
+//  RxOptional.h
+//  RxOptional
+//
+//  Created by Tyler Stromberg on 7/1/16.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RxOptional.
+FOUNDATION_EXPORT double RxOptionalVersionNumber;
+
+//! Project version string for RxOptional.
+FOUNDATION_EXPORT const unsigned char RxOptionalVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RxOptional/PublicHeader.h>
+
+
